8b9b3e11b19f69245540017f7088a88165970dea
Sirox228 <vladislawluzanow2@gmail.com>
2022-11-17 21:44:33 +0300

    upload

---
+++ build.sh
@@ -0,0 +1,19 @@
+echo "compiling for architecture: $1"
+
+cd v8/tools/dev
+
+case "$1" in
+    armv7-a)
+        python gm.py android_arm.release
+        ;;
+    armv8-a)
+        python gm.py android_arm64.release
+        ;;
+    *)
+        echo "you didn't specify an architecture or specified invalid one (available options: armv7-a, armv8-a)"
+        exit 1
+        ;;
+esac
+
+cd ../../
+ls

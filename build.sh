3e922ccbe6e2a69d6a84d0283aa61733d2509513
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-11-18 00:14:24 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,14 @@
 echo "compiling for architecture: $1"
 
-PWD=$(pwd)
-
 # export PATH="$PATH:$PWD/depot_tools/"
 
 # alias gn=$PWD/depot_tools/gn
 
 cd v8
-gclient sync
-python gsutil.py config
-./build/install-build-deps.sh
 
 # stupid way to fix can't find gn
-cp -r depot_tools/* v8
-rm v8/tools/dev/gm.py
+# cp -r depot_tools/* v8
+rm tools/dev/gm.py
 cp ../gm-modded.py tools/dev/gm.py
 
 # ./gn gen out/android_arm64.release
